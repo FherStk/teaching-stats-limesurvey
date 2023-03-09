6c98f79354a8deba04dac88409de7d6c89af5b9a
FherStk <porrino.fernando@gmail.com>
2023-03-09 01:12:46 +0100

    ready to implement

--- README.md
+++ README.md
@@ -5,14 +5,3 @@ This program prepares the teaching-stats reporting system, collecting up all nee
 
 ## Under developement
 The project is still under development and has not been completed yet. Please, refer to each release description for further information.
-
-## Requisites
-First, run the following SQL queries only if it's the first time running this script.
-```
-ALTER VIEW reports.answer RENAME TO answer_all;
-SELECT * INTO reports.answer FROM reports.answer_all;
-CREATE INDEX answer_year_idx ON reports.answer ("year");
-```
-
-### temp
- the main teaching-stats engine and also from a limesurvey source. In all cases, it moves the current answers into the traching-stats reporting historical tables.
---
+++ utils/JsonRPCclient.cs
@@ -0,0 +1,106 @@
+using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+ 
+//Source: https://manual.limesurvey.org/RemoteControl_2_API#How_to_use_LSRC2
+
+namespace JsonRPCclient {
+ 
+  public class JsonRPCclient {
+ 
+    private int id = 0;
+    /// <summary>
+    /// Set JSON-RPC webservice URL
+    /// </summary>
+    public string? URL { set; get; }
+    /// <summary>
+    /// Set JSON-RPC method
+    /// </summary>
+    public string? Method { set; get; }
+    /// <summary>
+    /// Add JSON-RPC params
+    /// </summary>
+    public JObject? Parameters { set; get; }
+ 
+    /// <summary>
+    /// Results of the request
+    /// </summary>
+    public JsonRPCresponse? Response { set; get; }
+ 
+ 
+    /// <summary>
+    /// Create a new object of RPCclient 
+    /// </summary>
+    public JsonRPCclient() {
+      Parameters = new JObject();
+      Response = null;
+    }
+ 
+    /// <summary>
+    /// Create a new object of RPCclient
+    /// </summary>
+    /// <param name="URL"></param>
+    public JsonRPCclient(string URL) {
+      this.URL = URL;
+      Parameters = new JObject();
+      Response = null;
+    }
+ 
+    /// <summary>
+    /// POST the request and returns server response
+    /// </summary>
+    /// <returns></returns>
+    public string Post() {
+      try {
+        JObject jobject = new JObject();
+        jobject.Add(new JProperty("jsonrpc", "2.0"));
+        jobject.Add(new JProperty("id", ++id));
+        jobject.Add(new JProperty("method", Method));
+        jobject.Add(new JProperty("params", Parameters));
+        
+        var httpClient = new HttpClient();
+        var asyncPost = httpClient.PostAsync(URL, new StringContent(JsonConvert.SerializeObject(jobject), Encoding.UTF8, "application/json"));    
+        asyncPost.Wait();
+        asyncPost.Result.EnsureSuccessStatusCode();
+
+        var asyncRead = asyncPost.Result.Content.ReadAsStringAsync();
+        asyncRead.Wait();        
+ 
+        Response = new JsonRPCresponse();
+        Response = JsonConvert.DeserializeObject<JsonRPCresponse>(asyncRead.Result);
+        if(Response != null){
+            Response.StatusCode = asyncPost.Result.StatusCode;
+            return Response.ToString();
+        }
+ 
+        return string.Empty;
+      }
+      catch (Exception e) {
+        return e.ToString();
+      }
+    }
+ 
+    public void ClearParameters() {
+      this.Parameters = new JObject();
+    }
+ 
+ 
+  }
+ 
+  public class JsonRPCresponse {
+    public int id { set; get; }
+    public object? result { set; get; }
+    public string? error { set; get; }
+    public HttpStatusCode StatusCode { set; get; }
+ 
+    //public JsonRPCresponse() { }
+ 
+    public override string ToString() {
+      return "{\"id\":" + id.ToString() + ",\"result\":\"" + (result ?? "").ToString() + "\",\"error\":" + error + ((String.IsNullOrEmpty(error)) ? "null" : "\"" + error + "\"") + "}";
+    }
+  }
+ 
+}
