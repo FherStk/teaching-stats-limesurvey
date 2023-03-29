6e9ff0bad77e2b628fda5959e03b6367a29032de
FherStk <porrino.fernando@gmail.com>
2023-03-29 11:03:04 +0200

    fixes

--- models/Survey.cs
+++ models/Survey.cs
@@ -14,8 +14,8 @@ public class Survey
     }
 
     public class Participant{
-        public string? Name {get; set;}
-        public string? Surname {get; set;}
+        public string? Firstname {get; set;}
+        public string? Lastname {get; set;}
         public string? Email {get; set;}
     }
 }
