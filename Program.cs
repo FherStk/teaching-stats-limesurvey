bc18dc786482ebe9f57aaf19bb2e75993339c9d3
FherStk <porrino.fernando@gmail.com>
2024-04-15 14:43:40 +0200

    Setting up teacher's surveys

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@
 
 internal class Program
 {
-    public const string Version = "2023-2024.1.0";
+    public const string Version = "2023-2024.1.1";
     public static Dictionary<Survey.Participant, List<Settings.SubjectData>>? EnrollmentWarnings {get; private set;}
 
     static void Main(string[] args)
